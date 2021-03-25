fc4c2748f6d6b385e22c2564c08fbfb96cbbaa4c
vexamd <65162246+vexamd@users.noreply.github.com>
2021-03-25 19:43:30 +0200

    Create fire.py

---
+++ fire.py
@@ -0,0 +1,32 @@
+
+import threading
+class Cub:
+    def __init__(self, num):
+        self.num = num
+        print("Un cub cu latura de {}".format(self.num))
+
+    def calc_volum(self):
+        return "Volumul cubului = {}".format(self.num * self.num * self.num)
+
+    def lenght_lat(self):
+        self.lenght = self.num * 12
+        return "Lungimea totala a tuturor laturilor = {}".format(self.lenght)
+
+    if __name__ == "__main__":
+        t1 = threading.Thread(target=lenght_lat, args=())
+        t2 = threading.Thread(target=calc_volum, args=())
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+
+cub_1 = Cub(10)
+print(cub_1.lenght_lat())
+print(cub_1.calc_volum())
+cub_2 = Cub(12)
+print(cub_2.lenght_lat())
+print(cub_2.calc_volum())
+print("Finalizare!")
